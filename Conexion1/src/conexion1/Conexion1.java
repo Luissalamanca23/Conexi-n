ec573354dd3e90c9ed99c56619e86a55b6911bed
Luis Salamanca <l.salamanca18@icloud.com>
2023-11-09 10:57:47 -0300

    conexio12

---
+++ Conexion1/src/conexion1/Conexion1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conexion1;
+
+/**
+ *
+ * @author luisslamanca
+ */
+public class Conexion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConexionBD conexion1 = new ConexionBD();
+        conexion1.Conexion();
+    }
+    
+}
---
+++ Conexion1/src/conexion1/ConexionBD.java
@@ -0,0 +1,24 @@
+package conexion1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    public String Conexion(){
+        String url="jdbc:mysql://localhost:3306/libreria";
+        String usuario="root";
+        String contraseña="Lola202319*@";
+
+        try{
+            Connection conexion=DriverManager.getConnection(url,usuario,contraseña);
+            if(conexion!=null){
+                System.out.println("Conexion exitosa");
+            }
+        } catch (SQLException ex){
+            System.out.println(ex);
+        }
+        return null;
+    }
+}
